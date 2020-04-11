b1b5b1bcf16edb06e7f986fa2a152bb73d9acee7
Phill Anderson <eaglesquad86@gmail.com>
2020-04-11 10:45:17 +0800

    Бургерийн орцыг динамикаар дүрслэв

--- src/pages/BurgerBuilder/index.js
+++ src/pages/BurgerBuilder/index.js
@@ -3,10 +3,19 @@ import React, { Component } from "react";
 import Burger from "../../components/Burger";
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 2,
+      salad: 2,
+      meat: 1,
+    },
+  };
+
   render() {
     return (
       <div>
-        <Burger />
+        <Burger orts={this.state.ingredients} />
         <div>Орцны удирдлага</div>
       </div>
     );
