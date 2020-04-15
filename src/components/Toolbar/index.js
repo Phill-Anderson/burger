0b0229dfa17e6bc8f41ca9ca2ef41f9e91b76937
Phill Anderson <eaglesquad86@gmail.com>
2020-04-15 17:02:10 +0800

    Menu, MenuItem компонэнтуудыг үүсгэв

--- src/components/Toolbar/index.js
+++ src/components/Toolbar/index.js
@@ -2,11 +2,14 @@ import React from "react";
 
 import css from "./style.module.css";
 import Logo from "../Logo";
+import Menu from "../Menu";
 const Toolbar = () => (
   <header className={css.Toolbar}>
     <div>...</div>
     <Logo />
-    <nav>Цэс</nav>
+    <nav>
+      <Menu />
+    </nav>
   </header>
 );
 
