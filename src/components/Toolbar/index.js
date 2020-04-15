292ee59e869752d8c4ab0c4cae1522746ca22a5d
Phill Anderson <eaglesquad86@gmail.com>
2020-04-15 16:39:28 +0800

    Лого компонэнтийг хийв

--- src/components/Toolbar/index.js
+++ src/components/Toolbar/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 import css from "./style.module.css";
-
+import Logo from "../Logo";
 const Toolbar = () => (
   <header className={css.Toolbar}>
     <div>...</div>
-    <div>Лого</div>
+    <Logo />
     <nav>Цэс</nav>
   </header>
 );
