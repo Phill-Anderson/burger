673a08ac0b78d34905e9c65fd7ff99228bd49586
Phill Anderson <eaglesquad86@gmail.com>
2020-04-15 17:07:36 +0800

    Menu , Menu Item компонэнтуудыг үүсгэнэ

---
+++ src/components/Logo/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import css from "./style.module.css";
+import logoImage from "../../assets/images/burger-logo.png";
+const Logo = () => (
+  <div className={css.Logo}>
+    <img src={logoImage} />
+  </div>
+);
+
+export default Logo;
---
+++ src/components/Menu/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import MenuItem from "../MenuItem";
+import css from "./style.module.css";
+const Menu = () => (
+  <div>
+    <ul className={css.Menu}>
+      <MenuItem active link="/">
+        Бургер
+      </MenuItem>
+      <MenuItem link="/checkout">Төлбөр</MenuItem>
+    </ul>
+  </div>
+);
+export default Menu;
