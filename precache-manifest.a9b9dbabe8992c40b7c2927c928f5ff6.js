5be37308f0d6dce2e64241b3ef94deaec2964f24
Phill Anderson <eaglesquad86@gmail.com>
2020-04-29 08:12:26 +0800

    Updates

---
+++ precache-manifest.a9b9dbabe8992c40b7c2927c928f5ff6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78a0fd4fa791bd350fbeb402ebfb3942",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eab943b435d035b87eb8",
+    "url": "/static/css/main.81e95883.chunk.css"
+  },
+  {
+    "revision": "565ef2984578be81d819",
+    "url": "/static/js/2.968537bd.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.968537bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eab943b435d035b87eb8",
+    "url": "/static/js/main.5f8335a4.chunk.js"
+  },
+  {
+    "revision": "172116f184c4321f02df",
+    "url": "/static/js/runtime-main.76bbe192.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);
