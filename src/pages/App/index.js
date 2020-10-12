5e3d6df23733aea5245a4941157b3380bd4dd626
Phill Anderson <eaglesquad86@gmail.com>
2020-10-12 18:48:29 +0800

    React.Lazy : js файлыг хэрэгтэй үед нь л дуудаж авчирна

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -1,27 +1,34 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Suspense } from "react";
 import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import css from "./style.module.css";
-
 import Toolbar from "../../components/Toolbar";
-import BurgerPage from "../BurgerPage";
 import SideBar from "../../components/SideBar";
-import OrderPage from "../OrderPage";
 import { Route, Switch } from "react-router-dom";
 import ShippingPage from "../ShippingPage";
 import Burger from "../../components/Burger";
 import LoginPage from "../LoginPage";
-import SignupPage from "../SignupPage";
 import Logout from "../../components/Logout";
 import * as actions from "../../redux/actions/loginActions";
 import * as signupActions from "../../redux/actions/signupActions";
+
+const BurgerPage = React.lazy(() => {
+  return import("../BurgerPage");
+});
+
+const OrderPage = React.lazy(() => {
+  return import("../OrderPage");
+});
+
+const SignupPage = React.lazy(() => {
+  return import("../SignupPage");
+});
 
 const App = props => {
   const [showSidebar, setShowSidebar] = useState(false);
 
   const toggleSideBar = () => {
     setShowSidebar(prevShowSidebar => !prevShowSidebar);
-    // prevShowSidebar нь render хийгдэхээс өмнөх showSidebar state - ийн утгыг заана
   };
 
   useEffect(() => {
@@ -54,20 +61,22 @@ const App = props => {
       <SideBar showSidebar={showSidebar} toggleSideBar={toggleSideBar} />
 
       <main className={css.Content}>
-        {props.userId ? (
-          <Switch>
-            <Route path="/logout" component={Logout} />
-            <Route path="/orders" component={OrderPage} />
-            <Route path="/ship" component={ShippingPage} />
-            <Route path="/" component={BurgerPage} />
-          </Switch>
-        ) : (
+        <Suspense fallback={<div>Түр хүлээнэ үү...</div>}>
+          {props.userId ? (
+            <Switch>
+              <Route path="/logout" component={Logout} />
+              <Route path="/orders" component={OrderPage} />
+              <Route path="/ship" component={ShippingPage} />
+              <Route path="/" component={BurgerPage} />
+            </Switch>
+          ) : (
             <Switch>
               <Route path="/signup" component={SignupPage} />
               <Route path="/login" component={LoginPage} />
               <Redirect to="/login" />
             </Switch>
           )}
+        </Suspense>
       </main>
     </div>
   );
